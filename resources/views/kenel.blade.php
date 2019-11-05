0f18b5b128cbffa15515965d09aa3b82a441a0b3
Phillip Jackson <unisys12@gmail.com>
2019-11-04 18:05:58 -0600

    remove bootstrap add base

--- resources/views/kenel.blade.php
+++ resources/views/kenel.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<nav class="container">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+</nav>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
